7b80f2db551974fced1a9b355e073c308cc67cc3
Adam Beatty <adam.t.beatt@gmail.com>
2020-08-19 23:17:09 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,9 +1,16 @@
 require 'pry'
 
 def my_all?(collection)
-  counter = 0
-  while counter < collection.length
-    yield(collection[counter])
-    counter += 1
+  i = 0
+  block_return_values = []
+  while i < collection.length
+    block_return_values << yield(collection[i])
+    i = i + 1
+  end
+
+  if block_return_values.include?(false)
+    false
+  else
+    true
   end
 end
